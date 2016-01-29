b7d92133891f70d9b3e395ae734a9c59888836bb
haits293 <haits293@gmail.com>
2016-01-29 23:15:08 +0700

    Almost done

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,7 +6,7 @@ class UserTest < ActiveSupport::TestCase
   # end
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-    				 password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -57,5 +57,9 @@ class UserTest < ActiveSupport::TestCase
      @user.password = @user.password_confirmation = "a" * 5
      assert_not @user.valid?
    end
+
+   test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 
 end
