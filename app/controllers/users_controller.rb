42bc0c92a4433a66201facfab23ecc71fb073316
haits293 <haits293@gmail.com>
2016-02-03 18:13:05 +0700

    Pending

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,26 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-def show
-@user = User.find(params[:id])
-=======
+
   def show
     @user = User.find(params[:id])
   end
 
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   private
@@ -27,26 +29,4 @@ def show
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
->>>>>>> sign-up
-end
-def new
-@user = User.new
-end
-def create
-@user = User.new(user_params)if @user.save
-log_in @user
-flash[:success] = "Welcome to the Sample App!"
-redirect_to @user
-else
-render 'new'
-end
-end
-def edit
-@user = User.find(params[:id])
-end
-private
-def user_params
-params.require(:user).permit(:name, :email, :password,
-:password_confirmation)
-end
 end
