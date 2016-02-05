6a89ff716576a1fc9d7a270f8d54f84d29c44e1c
haits293 <haits293@gmail.com>
2016-02-05 22:38:29 +0700

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -41,27 +41,52 @@ class UserTest < ActiveSupport::TestCase
       end
   end
 
-   test "email addresses should be unique" do
+  test "email addresses should be unique" do
      duplicate_user = @user.dup
      duplicate_user.email = @user.email.upcase
      @user.save
      assert_not duplicate_user.valid?
 	end
 
-   test "password should be present (nonblank)" do
+  test "password should be present (nonblank)" do
      @user.password = @user.password_confirmation = " " * 6
      assert_not @user.valid?
-   end
+  end
 
-   test "password should have a minimum length" do
+  test "password should have a minimum length" do
      @user.password = @user.password_confirmation = "a" * 5
      assert_not @user.valid?
-   end
+  end
 
-   test "authenticated? should return false for a user with nil digest" do
+  test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
-   end
-
-
+  end
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 
 end
